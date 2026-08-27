bac449d5a76402af597658b17f0c853babbc2b8b
lwk <145364509+nodlessoup@users.noreply.github.com>
2026-08-27 08:08:36 -0300

    .

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ const perguntas = [
   {
     enunciado:
       "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
+    alternativas: [
+        {
+          texto:"Isso é assustador!"
+          afirmacao: "afirmacao"
+        }
+        
+        {
+          texto: "Isso é maravilhoso!"
+          afirmacao:"afirmacao"
+        }, 
+    ],
   },
   {
     enunciado:
